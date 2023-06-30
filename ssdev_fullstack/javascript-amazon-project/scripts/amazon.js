93b3557b8a97db70294444006c900ab9e090aa34
Alina Chen <ajcheng7@gmail.com>
2023-06-30 11:53:41 -0700

    Pulls quantity from selector, can add multiple items

--- ssdev_fullstack/javascript-amazon-project/scripts/amazon.js
+++ ssdev_fullstack/javascript-amazon-project/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-            <select>
+            <select class="js-product-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -61,11 +61,14 @@ console.log(productsHTML);
 document.querySelector('.js-products-grid')
     .innerHTML = productsHTML;
 
+
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
+            const productQuantity = Number(document.querySelector(`.js-product-quantity-selector-${productId}`).value);
 
+            console.log(productQuantity.value);
             let matchingItem;
 
             cart.forEach((item) => {
@@ -75,11 +78,11 @@ document.querySelectorAll('.js-add-to-cart')
             });
 
             if (matchingItem) {
-                matchingItem.quantity += 1;
+                matchingItem.quantity += productQuantity;
             } else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    quantity: productQuantity
                 });
             }
 
