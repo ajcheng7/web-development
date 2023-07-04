76df5a9f543cd8420d22511429a327bd359164f9
Alina Chen <ajcheng7@gmail.com>
2023-07-03 17:50:10 -0700

    created bloglist to render blogs, useffect/usestate

---
+++ nnreact_tutorial/dojo-blog/src/BlogList.js
@@ -0,0 +1,26 @@
+const BlogList = ({blogs, title, handleDelete}) => {
+    // allows us to pass in data from the home.js file
+    // instead of doing it like this (below), we can destructure data in the parameter
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+
+    return ( 
+        <div className="blog-list">
+            <h2>{ title }</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                    <p>{ blog.body }</p>
+                    <button onClick={() => handleDelete(blog.id)}>
+                        delete blog
+                    </button>
+                </div>
+            ))}
+        </div>
+
+     );
+}
+ 
+export default BlogList;
--- nnreact_tutorial/dojo-blog/src/Home.js
+++ nnreact_tutorial/dojo-blog/src/Home.js
@@ -1,25 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     // e is a default parameter describing the event object
     // array destructuring to get two values, name of var and func to change it
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
     const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const handleClick = (e) => {
-        setName('luigi');
-        setAge('30');
+
+    // // create the function here instead of blogList so we can directly edit the data
+    // // then pass in the edited data as a prop
+    const handleDelete = (id) => {
+        // doesn't change the original data, returns new
+        const newBlogs = blogs.filter(blog=> blog.id !== id);
+        setBlogs(newBlogs);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
-    }
+    // // be careful changing state in useEffect bc might be an infinite loop
+    useEffect(() => {
+        console.log('use effect ran');
+        // console.log(blogs);
+        console.log(name);
+    }, [name]);
+    
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-            {/* write func that needs to pass in a parameter */}
-            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
+            {/* by passing in the blogs components we can now use this code anywhere */}
+            {/* we can also pass in functions as props */}
+           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> 
+           <button onClick={() => setName('luigi')}>change name</button>
+           <p>{ name }</p>
+           {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs" /> */}
         </div>
     );
 }
