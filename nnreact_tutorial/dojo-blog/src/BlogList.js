8e0f6c66b60fa950a6efcc60d9e6614af74d7361
Alina Chen <ajcheng7@gmail.com>
2023-07-07 15:05:36 -0700

    imports data from firebase now using useEffect

--- nnreact_tutorial/dojo-blog/src/BlogList.js
+++ nnreact_tutorial/dojo-blog/src/BlogList.js
@@ -6,11 +6,13 @@ const BlogList = ({blogs, title}) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
-
+    console.log(blogs);
+ 
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map((blog) => (
+                // console.log(blog.author)
                 <div className="blog-preview" key={blog.id}>
                     <Link to={`/blogs/${blog.id}`}>
                         <h2>{ blog.title }</h2>
--- nnreact_tutorial/dojo-blog/src/Home.js
+++ nnreact_tutorial/dojo-blog/src/Home.js
@@ -1,25 +1,76 @@
 import BlogList from './BlogList';
 import useFetch from "./useFetch";
+import { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { 
+  getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
 
 const Home = () => {
     // e is a default parameter describing the event object
     // array destructuring to get two values, name of var and func to change it
-    // const [blogs, setBlogs] = useState([
-    //     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    //     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    //     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    //   ]);
-    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
-    // be careful changing state in useEffect bc might be an infinite loop
-    // no dependency array so it will only run once
+    
+    // const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-    // create the function here instead of blogList so we can directly edit the data
-    // then pass in the edited data as a prop
-    // const handleDelete = (id) => {
-    //     // doesn't change the original data, returns new
-    //     const newBlogs = blogs.filter(blog=> blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
+    const firebaseConfig = {
+        apiKey: "AIzaSyDTAMs4XTRIjO5sYVoIi3honkdNEFcLb1Y",
+        authDomain: "fir-9-dojo-44ece.firebaseapp.com",
+        projectId: "fir-9-dojo-44ece",
+        storageBucket: "fir-9-dojo-44ece.appspot.com",
+        messagingSenderId: "428813948395",
+        appId: "1:428813948395:web:afedf1089dcb48651275ae"
+      };
+      
+    // initialize firebase app
+    initializeApp(firebaseConfig);
+    
+    // init services
+    const db = getFirestore();
+    
+    // collection ref
+    const colRef = collection(db, 'blogs')
+      
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    // get collection data
+    // getDocs(colRef)
+    //     .then((snapshot) => {
+    //         let data = [];
+    //         snapshot.docs.forEach((doc) => {
+    //             data.push({ ...doc.data(), id: doc.id });
+    //         })
+    //         console.log(data);
+    //         setBlogs(data);
+    //         setIsPending(false);
+    //         setError(null)
+    //     })
+    //     .catch(err => {
+    //         console.log(err.message);
+    //     });      
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const snapshot = await getDocs(colRef);
+            const data = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            console.log(data);
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+          } catch (err) {
+            console.log(err.message);
+            setError(err.message);
+            setIsPending(false);
+          }
+        };
+    
+        fetchData();
+      }, []);
+   
     
     return (
         <div className="home">
